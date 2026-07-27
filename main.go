05c15456f965f09d64198b3e6a5cbcd8fc6d693d
Thales Raymond <32554150+thalesraymond@users.noreply.github.com>
2026-07-27 18:56:08 -0300

    feat(server): Add metrics middleware and reset handler for file server

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"sync/atomic"
 
 	"github.com/thalesraymond/go-http-server/internal/handler"
 )
@@ -15,9 +17,39 @@ func main() {
 		Handler: serverMux,
 	}
 
-	serverMux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("./"))))
+	apiConfig := &apiConfig{}
+
+	serverMux.Handle("/app/", apiConfig.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir("./")))))
 
 	serverMux.HandleFunc("/healthz", handler.HealthzHandler)
 
+	serverMux.HandleFunc("/metrics", apiConfig.handlerMetrics)
+
+	serverMux.HandleFunc("/reset", apiConfig.handlerReset)
+
 	server.ListenAndServe()
+}
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write(fmt.Appendf(nil, "Hits: %d", cfg.fileserverHits.Load()))
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
 }
